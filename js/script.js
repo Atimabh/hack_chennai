a6906b843523b377f9e837618cbcbe0c2ae29527
atimabh <abarunaha@gmail.com>
2020-06-29 06:49:41 +0530

    minor

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var generateHere = document.getElementById("dynamicCards");
 
 for (var i = 0; i < len; i++) {
 
-    var html = '<div class="col-md-3"><div class="mentorCard"><div class="mentorImage"><img src="images/mentors/' + img[i] + '"></div><div class="mentorText"><h1 class="text-center">' + mentor[i] + '</h1><a href="#"><div class="linkedin"><i class="fab fa-linkedin-in"></i></div></a></div><div class="mentorSkills">' + skills[i] + '</div></div></div>';
+    var html = '<div class="col-lg-3 cardWrapper"><div class="mentorCard"><div class="mentorImage"><img src="images/mentors/' + img[i] + '"></div><div class="mentorText"><h1 class="text-center">' + mentor[i] + '</h1><a href="#"><div class="linkedin"><i class="fab fa-linkedin-in"></i></div></a></div><div class="mentorSkills">' + skills[i] + '</div></div></div>';
     generateHere.insertAdjacentHTML('beforeend', html);
     console.log(i);
 };
