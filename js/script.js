a35be1e36692c3de8befd31f5cba8671142b7bdf
atimabh <abarunaha@gmail.com>
2020-06-29 11:32:06 +0530

    added organizers

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var mentor = ['Anush Bhatia', 'Smaranjit Ghose'];
-var img = ['anush.png', 'smaranjit.png'];
-var skills = ['<span class="pink">UI/UX</span><span class="green">Cloud</span>', '<span class="pink">Python</span><span class="green">AI/ML</span><div class="break"></div><span class="yellow">CV</span>']
+var mentor = ['Aaishika S Bhattacharya', 'Anush Bhatia', 'Smaranjit Ghose', 'Ishika Gupta', 'Naynika Madan', 'Kushagra Singh', 'Atimabh Barunaha'];
+var img = ['naruto.webp', 'anush.png', 'smaranjit.png', 'naruto.webp', 'naruto.webp', 'naruto.webp', 'naruto.webp'];
+var position = ['- Team Lead', '- Creatives Lead', '- Technical Lead', '- Content Lead', '- Community Lead', '- Partnership Lead', '- Web Team Lead']
 var len = mentor.length;
 
 var generateHere = document.getElementById("dynamicCards");
 
 for (var i = 0; i < len; i++) {
 
-    var html = '<div class="col-lg-3 cardWrapper"><div class="mentorCard"><div class="mentorImage"><img src="images/mentors/' + img[i] + '"></div><div class="mentorText"><h1 class="text-center">' + mentor[i] + '</h1><a href="#"><div class="linkedin"><i class="fab fa-linkedin-in"></i></div></a></div><div class="mentorSkills">' + skills[i] + '</div></div></div>';
+    var html = '<div class="col-lg-3 cardWrapper"><div class="mentorCard"><div class="mentorImage"><img src="images/mentors/' + img[i] + '"><a href="#"><div class="linkedin"><i class="fab fa-linkedin-in"></i></div></a></div><div class="mentorText"><h1 class="text-center">' + mentor[i] + '</h1><h2 class="text-center">' + position[i] + '</h2></div></div></div>';
     generateHere.insertAdjacentHTML('beforeend', html);
     console.log(i);
 };
