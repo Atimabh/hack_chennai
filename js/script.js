6e2b3d80831fab799d50c6d095b25f37c740b06a
atimabh <abarunaha@gmail.com>
2020-07-15 12:07:01 +0530

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Mentors
-var mentor = ['Anush Bhatia', 'Smaranjit Ghose'];
+/*var mentor = ['Anush Bhatia', 'Smaranjit Ghose'];
 var img = ['naruto.webp', 'anush.png', 'smaranjit.png', 'naruto.webp', 'naruto.webp', 'naruto.webp', 'naruto.webp', 'naruto.webp'];
 var skills = ['<h1 class="pink">UI/UX</h1><h1 class="green">CLOUD</h1>', '<h1 class="pink">PYTHON</h1><h1 class="green">AI/ML</h1><h1 class="yellow">COMPUTER VISION</h1>']
 var len = mentor.length;
@@ -11,20 +11,36 @@ for (var i = 0; i < len; i++) {
     var html = '<div class="col-lg-3 cardWrapper"><div class="mentorCard"><div class="mentorImage"><img src="images/mentors/' + img[i] + '"><a href="#"><div class="linkedin"><i class="fab fa-linkedin-in"></i></div></a></div><div class="mentorText"><h1 class="text-center">' + mentor[i] + '</h1><div class="skills">' + skills[i] + '</div></div></div></div>';
     generateHere.insertAdjacentHTML('beforeend', html);
     console.log(i);
-};
+};*/
 
 //Team
-var member = ['Aaishika S Bhattacharya', 'Anush Bhatia', 'Smaranjit Ghose', 'Ishika Gupta', 'Naynika Madan', 'Kushagra Singh', 'Mayank Goyal', 'Atimabh Barunaha'];
-var position = ['Team Lead', 'creatives lead', 'technical lead', 'content lead', 'community lead', 'partnership lead', 'partnership lead', 'Web Team Lead'];
-var photo = ['naruto.webp', 'anush.png', 'smaranjit.png', 'naruto.webp', 'naruto.webp', 'naruto.webp', 'naruto.webp', 'naruto.webp'];
+var member = ['Aaishika S Bhattacharya', 'Anush Bhatia', 'Smaranjit Ghose', 'Mayank Goyal', 'Atimabh Barunaha'];
+var position = ['Team Lead', 'creatives lead', 'technical lead', 'partnership lead', 'Web Team Lead'];
+var photo = ['aaishika.jpeg', 'anush.png', 'smaranjit.png', 'mayank.jpg', 'atimabh.jpg'];
+var github = ['https://github.com/aaishikasb', 'https://github.com/anushbhatia', 'https://github.com/smaranjitghose', 'https://github.com/heromayank2', 'https://github.com/Atimabh'];
+var linkedin = ['https://www.linkedin.com/in/aaishika', 'https://www.linkedin.com/in/anushbhatia/', 'https://www.linkedin.com/in/smaranjitghose/', 'https://www.linkedin.com/in/mayankkgoyal/', 'https://www.linkedin.com/in/barunaha/'];
 var he = member.length;
 console.log(he);
 
 var dynamic = document.getElementById("teamCards");
 
 for (var j = 0; j < he; j++) {
-    var content = ' <div class="col-lg-3 cardWrapper"><div class="teamCard"><div class="teamImage"><img src="images/mentors/' + photo[j] + '" alt=" "></div><div class="teamText"><h1>' + member[j] + '</h1><h2>' + position[j] + '</h2></div><div class="teamLinks"><a href="#"><div class="teamLinkedin"><img src="images/linkedin.svg" alt=" "></div></a><a href="#"><div class="teamGithub"><img src="images/github.svg" alt=" "></div></a></div></div></div>';
+    var content = ' <div class="col-lg-3 cardWrapper"><div class="teamCard"><div class="teamImage"><img src="images/team/' + photo[j] + '" alt=" "></div><div class="teamText"><h1>' + member[j] + '</h1><h2>' + position[j] + '</h2></div><div class="teamLinks"><a href="' + linkedin[j] + '"><div class="teamLinkedin"><img src="images/linkedin.svg" alt=" "></div></a><a href="' + github[j] + '"><div class="teamGithub"><img src="images/github.svg" alt=" "></div></a></div></div></div>';
     dynamic.insertAdjacentHTML('beforeend', content);
+};
+
+//Team No Github
+var nogit = ['Ishika Gupta', 'Naynika Madan', 'Kushagra Singh'];
+var nogitpos = ['content lead', 'community lead', 'partnership lead'];
+var nogitpic = ['ishika.jpeg', 'naynika.jpg', 'kushagra.jpg'];
+var nogitlinkedin = ['https://www.linkedin.com/in/findishika/', 'https://www.linkedin.com/in/naynikamadan', 'https://www.linkedin.com/in/kushagra-singh-160b8518b'];
+var srmhe = nogit.length;
+
+var teamnogit = document.getElementById('teamCards');
+
+for (var k = 0; k < srmhe; k++) {
+    var hackchennai = ' <div class="col-lg-3 cardWrapper"><div class="teamCard"><div class="teamImage"><img src="images/team/' + nogitpic[k] + '" alt=" "></div><div class="teamText"><h1>' + nogit[k] + '</h1><h2>' + nogitpos[k] + '</h2></div><div class="teamLinks"><a href="' + nogitlinkedin[k] + '"><div class="teamLinkedin"><img src="images/linkedin.svg" alt=" "></div></a></div></div></div>';
+    dynamic.insertAdjacentHTML('beforeend', hackchennai);
 };
 
 //Navbar
@@ -89,6 +105,6 @@ function initializeClock(id, endtime) {
     const timeinterval = setInterval(updateClock, 1000);
 }
 
-const deadline = new Date(Date.parse(new Date('August 14 2020')));
+const deadline = new Date(Date.parse(new Date('August 22 2020')));
 // const deadline = '2020-11-14'
 initializeClock('clockdiv', deadline);
